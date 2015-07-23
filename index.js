91f48bb0d952c4474842423e79138a34267003e4
Витя <likenoother1992@gmail.com>
2015-07-23 18:51:12 +0300

    new min max validate

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * Created by Витя on 23.07.2015.
  */
 function randomRange {
+    min= min  || 1;
+    max=  max || 1;
+
     return Math.floor(random()*(max-min)+min);
 }
 console.log(randomRange(1,20);
