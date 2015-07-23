5bfa042746c13f44578d5c28f9c970b660092f82
Витя <likenoother1992@gmail.com>
2015-07-23 19:12:20 +0300

    merge testing to master

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /**
  * Created by Витя on 23.07.2015.
  */
-function randomRange {
+function randomRange(min,max) {
     return Math.floor(random()*(max-min)+min);
 }
 for (var i=0; i<10; i++){
-    console.log(randomRange(1,20);
+    console.log(randomRange(1,20));
 }
-console.log(randomRange(1,20);
